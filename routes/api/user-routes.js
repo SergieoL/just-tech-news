e8db5d656cad2103fe68caac01a31ae5eefde60a
Sergieo <sergieol23@gmail.com>
2022-05-04 20:16:34 -0500

    Added Post Model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const bcrypt = require('bcrypt');
 
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our USer model and run .findALL() mdethod
     User.findAll({
-        attributes: { exclude: ['password']}
+        //attributes: { exclude: ['password']}
     })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -17,7 +18,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['password']},
+        //attributes: { exclude: ['password']},
         where: {
             id: req.params.id
         }
@@ -50,14 +51,38 @@ router.post('/', (req, res) => {
     })
 });
 
+// login route for authentication
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Wrong password dumby' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
-      where: {
-        id: req.params.id
+        individualHooks: true,
+        where: {
+            id: req.params.id
       }
     })
       .then(dbUserData => {
