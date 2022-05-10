669da0b68ce2994222c9cec5692b4d07a8b76e8d
Sergieo <sergieol23@gmail.com>
2022-05-10 00:06:26 -0500

    Added homepage view

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-// get all commenys
+// get all comments
 router.get('/', (req, res) => {
     Comment.findAll()
     .then(dbCommentData => res.json(dbCommentData))
